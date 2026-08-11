d2d97f9d9173c9b5925acc673431315df0d3ad1d
ljridley <ljridley@proton.me>
2026-08-11 09:34:12 +0100

    Update uninstallscreeconnect.ps1

--- Connectwise/uninstallscreeconnect.ps1
+++ Connectwise/uninstallscreeconnect.ps1
@@ -1,2 +1,3 @@
 # Change the ... in the brackets to your screenconnect instance code, can be found in list of apps in control panel > uninstall programs
-wmic product where name="ScreenConnect Client (...)" call uninstall /nointeractive
+# Or remove brackets and put * instead to target all screenconnect instances
+Get-Package -Name "ScreenConnect Client (...)" | Uninstall-Package
